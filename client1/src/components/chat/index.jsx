595752952372ff56bab30d0fed20d1833f0ed0cc
Agam18 <97403095+Agam18@users.noreply.github.com>
2024-08-01 16:46:55 +0530

    Update index.jsx

--- client1/src/components/chat/index.jsx
+++ client1/src/components/chat/index.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-chat-engine-advanced";
 import Header from "@/components/customHeader";
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
+
 import Ai from '@/components/customMessageForms/Ai';
 import AiCode from '@/components/customMessageForms/AiCode';
 import AiAssist from '@/components/customMessageForms/AiAssist';
